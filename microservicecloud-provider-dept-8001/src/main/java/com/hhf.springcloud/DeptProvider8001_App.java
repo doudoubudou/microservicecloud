fa51c889f6759228cd456fef8fadd01e8816a423
doufang <iedoufang@163.com>
2019-07-05 16:53:59 +0800

    eurake build

--- microservicecloud-provider-dept-8001/src/main/java/com/hhf.springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/hhf.springcloud/DeptProvider8001_App.java
@@ -2,7 +2,9 @@ package com.hhf.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DeptProvider8001_App {
     public static void main(String[] args) {
