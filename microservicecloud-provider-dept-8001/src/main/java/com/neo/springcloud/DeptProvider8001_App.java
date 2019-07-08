c3d113251c96c31d9540310a0ee7db9b9951c471
doufang <iedoufang@163.com>
2019-07-08 18:02:24 +0800

    8001服务启动问题修复

--- microservicecloud-provider-dept-8001/src/main/java/com/neo/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/neo/springcloud/DeptProvider8001_App.java
@@ -1,11 +1,17 @@
 package com.neo.springcloud;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@MapperScan("com.neo.springcloud.dao")
+@EnableAutoConfiguration
 public class DeptProvider8001_App {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider8001_App.class,args);
