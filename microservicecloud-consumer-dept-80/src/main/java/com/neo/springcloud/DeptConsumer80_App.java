b9c3bbbae09f8c403103707b84f52857274cbd00
doudoubudou <iedoufang@163.com>
2019-07-09 09:03:48 +0800

    eureka配置

--- microservicecloud-consumer-dept-80/src/main/java/com/neo/springcloud/DeptConsumer80_App.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/neo/springcloud/DeptConsumer80_App.java
@@ -2,9 +2,9 @@ package com.neo.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class DeptConsumer80_App {
     public static void main(String[] args) {
