2560681ea0b327a78d9743e2de51f92450cfd33c
doufang <iedoufang@163.com>
2019-07-10 13:54:03 +0800

    数据库配置修改

--- microservicecloud-provider-dept-8001/src/main/java/com/neo/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/neo/springcloud/DeptProvider8001_App.java
@@ -1,13 +1,13 @@
 package com.neo.springcloud;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureJdbc;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-@AutoConfigureJdbc
+@MapperScan(basePackages = "com.neo.springcloud.dao")
 public class DeptProvider8001_App {
 
     public static void main(String[] args) {
